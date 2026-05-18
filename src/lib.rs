1332252329e22eca031e9d3b6e9b471143aa5269
T4rp <why9ten@gmail.com>
2026-05-18 19:01:09 -0400

    feat: make freetype integration optional

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+pub mod harfbuzz;
 
-pub mod harfbuzz {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
-
+#[cfg(feature = "freetype")]
 pub mod freetype;
